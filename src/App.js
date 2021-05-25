0a68afba3d8ea5bcc1280ea28bb4913fb70e4ce9
Godoyeon <97godo@naver.com>
2021-05-26 01:45:21 +0900

    웹 페이지 목차 구성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import AppShell from './components/AppShell';
+import AppTab from './components/AppTab';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>1. AppShell 버전</p><AppShell />
+      <p>2. AppTab 버전</p><AppTab />
     </div>
   );
 }
