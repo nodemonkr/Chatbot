78c070f92b505ae9c6c515a4d1b783b925147531
GoDoyeon <97godo@naver.com>
2021-06-07 00:23:54 +0900

    2021-06-07 DY : chatbot css 수정
    team.js에 올린 파일 chatbot.js로 옮김

--- src/pages/chatbot.js
+++ src/pages/chatbot.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
-import "../css/chat.css"
+import styles from "../css/chat.module.css"
+import classNames from 'classnames'
 import {API} from "../api/api"
 
 
@@ -22,50 +23,78 @@ const Chatbot = () => {
 
 
     return (
-       <div>
-
-
-            <div className="align-items-center">
-        <div className="d-inline-flex flex-column border" id="chat">
-          <div className="card-body msg_card_body mt-1">
-          <div>2021년 5월 30일</div><br/>
-          <strong>하이밋</strong><br/>
-            <div className="d-flex justify-content-start  mb-4">
-            <img src='images/임시chaticon.png' width="40" height="40"></img>
-              <div className="msg_cotainer">
-                <p>받은 메시지</p>
-                <span className="msg_time">11시 40분</span>
-              </div>
-            </div>
-            <div className="d-flex justify-content-end  mb-4">
-              <div className="msg_cotainer_send">
-                <p>보낸 메시지</p>
-                <span className="msg_time_send">11시 45분</span>
-              </div>
-            </div>
+      <div className={styles.chatbot}>
+      {/* 왼쪽 챗봇 리스트 */}
+      <div className={styles.chatbotLeft}>
+          <div className={classNames({[styles.chatbotList]:true, [styles.chatbotFont]:true})}>
+              <ul>
+                  <li><p># 직업별 질문 챗봇</p></li>
+                  <li><p># 기업별 질문 챗봇</p></li>
+                  <li><p># 성격별 질문 챗봇</p></li>
+              </ul>
           </div>
-          <div className="card-footer">
-            <div className="input-group">
-              <input
-                className="form-control type_msg"
-                placeholder="메세지를 입력해주세요"
-                value={message}
-                onChange={(e)=>setMessage(e.target.value)}
-                // onKeyPress={}
-              />
-              <div className="input-group-append">
-                <span
-                  className="input-group-text send_btn "
-                  onClick={chatBotCall}
-                >
-                  send
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
-    </div>
+      {/* 오른쪽 챗봇 채팅 */}
+      <div className={styles.chatbotRight}>
+          {/* 대화 중인 챗봇 */}
+          <div className={classNames({[styles.chatbotFont]:true, [styles.chatbotName]:true})}>
+                 # 직업별 질문 챗봇
+          </div>
+          {/* 대화 내용 */}
+          <div className={styles.chatbotTalk}>
+              <div className={styles.center}>
+
+              <span className={styles.talkDay}>2021년 5월 30일</span>
+              </div>
+                  {/* 받은 메시지 */}
+                  <div className={styles.getTalk}>
+                      <span className={styles.chatbotImage}>
+                          <img src='images/임시chaticon.png' width="30" height="30"/>
+                      </span>
+                      <div claaName={styles.talkContainer}>
+                          <strong className={styles.talkChatbotName}>하이밋</strong><br/>
+                          
+                          <span className={styles.getTalkAbout}>
+                              <div className={classNames({[styles.getMsg]:true, [styles.chatbotFont]:true})}>
+                                  <p>받은 메시지</p>
+                              </div>  
+                              <div><p className={styles.talkTime}>11시 40분</p></div>
+                          </span>
+                      </div>
+                  </div>
+                   {/* 보낸 메시지 */}
+                   <div className={styles.sendTalk}>
+                       <span className={styles.sendTalkAbout}>
+                              <div><p className={styles.talkTime}>11시 40분</p></div>
+                              <div className={classNames({[styles.sendMsg]:true, [styles.chatbotFont]:true})}>
+                                  <p>보낸 메시지</p>
+                              </div>
+                      </span>
+                  </div>
+          </div>
+          {/* 톡 보내기 */}
+          <div className={styles.chatbotSandTalk}>
+                  <div className={styles.inputSandTalk}>
+                  <input
+                      className={styles.formControl}
+                      placeholder="메세지를 입력해주세요"
+                      //value={message}
+                      //onChange={(e)=>setMessage(e.target.value)}
+                      // onKeyPress={}
+                  />
+                  <div>
+                      <span
+                         className={styles.inputSendBox}
+                      // onClick={chatBotCall}
+                      >
+                      send
+                      </span>
+                  </div>
+                  </div> 
+              
+          </div>
+      </div>
+  </div>
 
 
 
--- src/pages/team.js
+++ src/pages/team.js
@@ -5,79 +5,9 @@ import classNames from 'classnames'
 
 const Team = () => {
     return (
-      
-        <div className={styles.chatbot}>
-            {/* 왼쪽 챗봇 리스트 */}
-            <div className={styles.chatbotLeft}>
-                <div className={classNames({[styles.chatbotList]:true, [styles.chatbotFont]:true})}>
-                    <ul>
-                        <li><p># 직업별 질문 챗봇</p></li>
-                        <li><p># 기업별 질문 챗봇</p></li>
-                        <li><p># 성격별 질문 챗봇</p></li>
-                    </ul>
-                </div>
+        <div>
+            d
             </div>
-            {/* 오른쪽 챗봇 채팅 */}
-            <div className={styles.chatbotRight}>
-                {/* 대화 중인 챗봇 */}
-                <div className={classNames({[styles.chatbotFont]:true, [styles.chatbotName]:true})}>
-                       # 직업별 질문 챗봇
-                </div>
-                {/* 대화 내용 */}
-                <div className={styles.chatbotTalk}>
-                    <div className={styles.center}>
-
-                    <span className={styles.talkDay}>2021년 5월 30일</span>
-                    </div>
-                        {/* 받은 메시지 */}
-                        <div className={styles.getTalk}>
-                            <span className={styles.chatbotImage}>
-                                <img src='images/임시chaticon.png' width="30" height="30"/>
-                            </span>
-                            <div claaName={styles.talkContainer}>
-                                <strong className={styles.talkChatbotName}>하이밋</strong><br/>
-                                
-                                <span className={styles.getTalkAbout}>
-                                    <div className={classNames({[styles.getMsg]:true, [styles.chatbotFont]:true})}>
-                                        <p>받은 메시지</p>
-                                    </div>  
-                                    <div><p className={styles.talkTime}>11시 40분</p></div>
-                                </span>
-                            </div>
-                        </div>
-                         {/* 보낸 메시지 */}
-                         <div className={styles.sendTalk}>
-                             <span className={styles.sendTalkAbout}>
-                                    <div><p className={styles.talkTime}>11시 40분</p></div>
-                                    <div className={classNames({[styles.sendMsg]:true, [styles.chatbotFont]:true})}>
-                                        <p>보낸 메시지</p>
-                                    </div>
-                            </span>
-                        </div>
-                </div>
-                {/* 톡 보내기 */}ㅞㅡ
-                <div className={styles.chatbotSandTalk}>
-                        <div className={styles.inputSandTalk}>
-                        <input
-                            className={styles.formControl}
-                            placeholder="메세지를 입력해주세요"
-                            //value={message}
-                            //onChange={(e)=>setMessage(e.target.value)}
-                            // onKeyPress={}
-                        />
-                        <div>
-                            <span
-                               className={styles.inputSendBox}
-                            // onClick={chatBotCall}
-                            >
-                            send
-                            </span>
-                        </div>
-                        </div> 
-                    
-                </div>
-            </div>
-        </div>
 
     )
 }
