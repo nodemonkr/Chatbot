0d51a7b9fe83858db237551c033d4659059d7419
GoDoyeon <97godo@naver.com>
2021-08-09 18:50:04 +0900

    feat : authform 박스 onchange, onsubmit 추가

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Button from '../common/Button';
 import palette from '../../lib/styles/palette';
+import { useState } from 'react';
 
 const textMap = {
   login: '로그인',
@@ -11,17 +12,51 @@ const textMap = {
 // 회원가입, 로그인 폼을 보여줍니다.
 const AuthForm = ({ type }) => {
   const text = textMap[type];
+  const [authObj, setAuthObj] = useState({
+    id: '',
+    password: '',
+    passwordConfirm: '',
+    username: '',
+  });
+
+  const onChange = async (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    setAuthObj((authObj) => ({ ...authObj, [name]: value }));
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      console.log('onSubmit');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  // const [id, setId] = React.useState('');
+  // const [password, setPassword] = React.useState('');
+  // const [passwordConfirm, setPasswordConfirm] = React.useState('');
+  // const [username, setUsername] = React.useState('');
+
   return (
     <AuthFormBlock>
       <h3>{text} </h3>
-      <form>
-        <StyledInput autoComplete='id' name='id' placeholder='아이디' />
+      <form onSubmit={onSubmit}>
+        <StyledInput
+          autoComplete='id'
+          name='id'
+          placeholder='아이디'
+          onChange={onChange}
+          value={authObj.id}
+        />
 
         <StyledInput
           autoComplete='new-password'
           name='password'
           placeholder='비밀번호'
           type='password'
+          onChange={onChange}
+          value={authObj.password}
         />
         {type === 'signUp' && (
           <>
@@ -30,17 +65,22 @@ const AuthForm = ({ type }) => {
               name='passwordConfirm'
               placeholder='비밀번호 확인'
               type='password'
+              onChange={onChange}
+              value={authObj.passwordConfirm}
             />
             <StyledInput
               autoComplete='username'
               name='username'
               placeholder='이름'
+              onChange={onChange}
+              value={authObj.username}
             />
           </>
         )}
         <ButtonWithMarginTop navy fullWidth style={{ marginTop: '2rem' }}>
           {text}
         </ButtonWithMarginTop>
+        <input type='submit' value={text} />
       </form>
       <Footer>
         {type === 'login' ? (
