ba9495e1cf9f9cd29c356c49a527798e87b2ad7a
GoDoyeon <97godo@naver.com>
2021-08-10 21:22:02 +0900

    style : 로그인, 회원가입 디자인 변경
    - 디자인 변경
    - 필요없는 파일 삭제

--- src/components/auth/AuthTemplate.js
+++ src/components/auth/AuthTemplate.js
@@ -1,18 +1,12 @@
 import styled from 'styled-components';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
 
 // 회원가입, 로그인 페이지의 레이아웃을 담당하는 컴포넌트
 const AuthTemplate = ({ children }) => {
   return (
     <AuthTemplateBlock>
-      <BaseBox>
-        <div className='logo-area'>
-          <Link to='/'>Himeeting</Link>
-        </div>
-        {children}
-      </BaseBox>
+      <BaseBox>{children}</BaseBox>
     </AuthTemplateBlock>
   );
 };
@@ -32,23 +26,6 @@ const AuthTemplateBlock = styled.div`
 const BaseBox = styled.div`
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.025);
   width: 30rem;
-  border-radius: 30px;
   border: 1px solid ${palette.navy[0]};
-  .logo-area {
-    padding-top: 1rem;
-    border-radius: 30px 30px 0 0;
-    display: block;
-    padding-bottom: 1rem;
-    text-align: center;
-    font-weight: bold;
-    letter-spacing: 1px;
-    border: none;
-    background-color: ${palette.navy[0]};
-    a {
-      color: ${palette.white[0]};
-      font-size: 1.5rem;
-      text-decoration: none;
-    }
-  }
 `;
 export default AuthTemplate;
